c2def65c39833380040c67b2602551ebc23b9ace
Henok <henokmicrometre@gmail.com>
2020-05-02 12:51:38 +0100

    adding systemd

--- scripts/ffmpeg-v4loop.sh
+++ scripts/ffmpeg-v4loop.sh
@@ -1,6 +1,6 @@
-ffmpeg -i samples/*.mp4 -f v4l2 -r 10 -pix_fmt yuyv422 /dev/video0 
 #!/bin/bash
 while true
 do 
-	ffmpeg -i samples/*.mp4 -f v4l2 -r 10 -pix_fmt yuyv422 /dev/video0 
+    ffmpeg -i /home/debo/repos/anpr/samples/2.mp4 -f v4l2 -r 10 -pix_fmt yuyv422 /dev/video0
+
 done
