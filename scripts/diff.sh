fcd2f8df6355f99f722a69112c68c7e6040494ae
Henok <henokmicrometre@gmail.com>
2020-05-19 16:32:28 +0100

    sorting http delete

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,2 +1,3 @@
-cat public/result.csv | grep reg | tr : "    " | awk {'print $3'} | tr -d '", }'
-
+#!/bin/bash
+cat public/db.json | grep reg | tr : "    " | awk {'print$3'} | tr -d '", }' 
+cat public/alprresult.json | grep reg | tr : "    " | awk {'print$2'} | tr -d '". }'
