0b707e0546cc07948ca706e20a583f5851745398
Henok <henokmicrometre@gmail.com>
2020-05-29 20:30:55 +0100

    parsing public/result.json

--- scripts/parse-resluts.sh
+++ scripts/parse-resluts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for f in public/*.json; do
-    cat public/alprresult.json | sed "s/null/{\""\registration\":"\"Absent\"}/g"  | jq '.' > public/result.json
+    cat public/alprresult.json | sed "s/null/{\""\registration\":"\"Absent\"}/g"  | jq -r '.' > public/result.json
 
 done   
