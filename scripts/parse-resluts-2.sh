8c94f640ade0bff4ab1503cd3b74de2de7a55a46
henok <henokmicromete@gmail.com>
2020-05-31 06:15:48 +0100

    db template

--- scripts/parse-resluts-2.sh
+++ scripts/parse-resluts-2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 for f in public/*.json; do
-     cat ~/repos/anpr/public/alprresult.json | sed "s/null/{\""\registration\":"\"Absent\"}/g"  | jq -r '.' | tr -d '{}, ,"' | sed s/_id/User/g | sed s/:/,/g > ~/repos/anpr/public/alprresult.csv
-
+     cat ~/repos/anpr/public/alprresult.json | sed "s/null/{\""\alprDB\":"\"Absent\"}/g"  | sed '/_id*/c\{"alprDB":"present"}' > public/result-filtered.json
 done   
-cat ~/repos/anpr/public/alprresult.json | sed "s/null/{\""\alprDB\":"\"Absent\"}/g"  | sed '/_id*/c\from_alprDB'
+cat /home/anpr/repos/anpr/public/alprresult.json | sed "s/null/{\""\Absent\":"\"Absent\"}/g" | sed 'N;s/\n/\t/' | tr -d '{}""' | sed s/:/,/ | sed 's/_id:/ /' | sed 's/:/   /' | sed 's/,/ /g' | awk {'print$1,$2,$3'}
