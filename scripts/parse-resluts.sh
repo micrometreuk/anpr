4cf8e3b8cfa2b71561f3f3b8289b9b11c73d69eb
Henok <henokmicrometre@gmail.com>
2020-06-06 02:54:27 +0100

    sorting regex filter

--- scripts/parse-resluts.sh
+++ scripts/parse-resluts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for f in public/*.json; do
-cat /home/debo/repos/anpr/public/alprresult.json | sed "s/null/{\"\     Absent\"\},/g" | sed 'N;s/\n/\t/' | tr -d '{}:"' | sed 's/,/  /' | awk {'print $1, $2'} | sed 's/registration//' | sed 's/_id/      ,_id:/' | sed 's/Absent/         ,Absent/'  | awk 'gsub(" ","")' | sed '1 i\Regstrations, Data base Id' | awk '{print}' > public/result.csv
+     cat /home/debo/repos/anpr/public/alprresult.json | sed 's/null/{"presence":\"\Absent\"\}/g' | sed '/_id/c\{"presence":\"\present\"\}'
 done   
