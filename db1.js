2b20bdfdeb2348d0912f10c22771ee2178227ff9
henok <henokmicrometre@gmail.com>
2021-03-08 15:44:55 +0000

    sorted mongo

---
+++ db1.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
