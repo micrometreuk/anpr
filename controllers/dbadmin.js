eb87da504413a1ddd9650ee8646bd4578c6e10a9
henok <henokmicrometre@gmail.com>
2021-03-24 12:28:49 +0000

    adding mongo models

--- controllers/dbadmin.js
+++ controllers/dbadmin.js
@@ -1,58 +1,15 @@
-var Alpr = require('../models/dbadmin');
+var Alpr = require('../models/alpr');
 
 exports.index = function(req, res) {
     res.render('pages/db.ejs');
    // res.sendFile(path.resolve('views/alprdb.html'));
 };
 
-exports.data = function(req, res) {
-    res.render('pages/agg.ejs');
-   // res.sendFile(path.resolve('views/alprdb.html'));
-};
-exports.create = async function(req, res) {
-   var newAlpr = new Alpr(req.body.results[0]);
-    try {
-        await newAlpr.save();
-        console.log(newAlpr);
-        res.send(newAlpr);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-};
-
-exports.delete = function(req, res) {
-  var item = req.body;
-   Alpr.remove({ _id: item._id }, {}, function(err) {
-   res.json(item);
-    });   
-  console.log(item);
-  }
-
-exports.put = function(req, res) {
-  var item = req.body;
-  Alpr.update({ _id: item._id }, item, {}, function(err) {
-   res.json(item);
-    });   
-  console.log(item);
-  }
-
-
 exports.list = async function(req, res) {
-var item = await Alpr.find(
-    {},
-    {plate: 1 }
-).sort({plate: 1 })
-   res.json(item);
-  console.log(item);
-};
-
-exports.agg = async function(req, res) {
-var item = await Alpr.aggregate ([
-{ $group: { _id: "$plate"} },
-{$sort:{_id : 1} },
-{$project: { _id: 0, plate: "$_id" }}
-])
-   res.json(item);
-  console.log(item);
+  Alpr.find({}, function(err, alprs) {
+      console.log(alprs);
+   res.json(alprs);
+  })
+  .sort({plate: 1});  
 };
 
--- models/dbadmin.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Dbadmin = new Schema ({
-        plate: { type: String},
-});
-module.exports = mongoose.model('Dbadmin', Dbadmin)
