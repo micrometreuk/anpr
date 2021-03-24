db75f8163ece49ac131dd5ac68e39d69d7664d2c
henok <henokmicrometre@gmail.com>
2021-03-24 16:22:28 +0000

    removing temp

--- controllers/dbadmin.js
+++ controllers/dbadmin.js
@@ -6,10 +6,12 @@ exports.index = function(req, res) {
 };
 
 
+
 exports.list = async function(req, res) {
 var item = await Alpr.aggregate ([
 { $group: { _id: "$plate"} },
-{$sort:{_id : 1} } 
+{$sort:{_id : 1} },
+{$project: { _id: 0, plate: "$_id" }}
 ])
    res.json(item);
   console.log(item);
