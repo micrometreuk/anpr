66fafa252bcfc6bfff59d90abdb679b885b5086a
Henok <henokmicrometre@gmail.com>
2021-03-30 10:00:42 +0100

    sorting scripts/openalpr-buster.sh

--- scripts/openalpr-buster.sh
+++ scripts/openalpr-buster.sh
@@ -13,15 +13,15 @@ libtesseract-dev \
 wget \
 beanstalkd \
 tesseract-ocr
-##  sudo apt upgrade -y
-##  # Clone the latest code from GitHub
-##  git clone https://github.com/openalpr/openalpr.git
-##  # Setup the build directory
-##  cd openalpr/src
-##  mkdir build
-##  cd build
-##  # setup the compile environment
-##  cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_INSTALL_SYSCONFDIR:PATH=/etc ..
-##  # compile the library
-##  make
+# Clone the latest code from GitHub
+git clone https://github.com/openalpr/openalpr.git
+# Setup the build directory
+cd openalpr/src
+mkdir build
+cd build
+# setup the compile environment
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_INSTALL_SYSCONFDIR:PATH=/etc ..
+# compile the library
+make
 # Install the binaries/libraries to your local system (prefix is /usr)
+make install
