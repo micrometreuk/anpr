52cbdbdc2e21e3ecd62cc31fe1bca0fabe951778
Henok Wehibe <henokwehibe@gmail.com>
2024-12-25 22:42:33 +0000

    rawHeaders: for host info

--- api/controllers/anpr.js
+++ api/controllers/anpr.js
@@ -5,8 +5,8 @@ exports.index = function(req, res) {
 };
 
 exports.create = async function(req, res) {
-    var anpr_host_protpcol = "https://"
-    var anpr_images_path = "/alprd_images/"
+    var anpr_host_protpcol = "http://"
+    var anpr_images_path = "/alprd-images/"
     var anpr_image = req.body.uuid + ".jpg"
     var anpr_host = req.rawHeaders[1]
          console.log(anpr_host_protpcol  + anpr_host + anpr_images_path + anpr_image);
