a9e675bc0b826a3b0f59c7dc71cb518b744f23dc
penguin <penguin@gmail.com>
2024-12-26 13:15:12 +0000

    added sse and testing express-sse lib

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var SSE = require('express-sse');
+
+ 
+const sse = new SSE();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/events1', (req, res) => {
+  //getting res.flush is not a function in express-sse https://github.com/dpskvn/express-sse/issues/28 
+  res.flush = function () { /* Do nothing */ }
+  sse.init(req, res); 
+  const interval =  setInterval(() => {
+    const randomData = Math.random();
+    sse.send({ message: 'Hello from SSE!' }); 
+    //sse.send({ message: randomData }); 
+  }, 2000); 
+  req.on('close', () => {
+    //sse.close(); 
+    clearInterval(interval); 
+  });
 });
 
 
@@ -16,8 +37,8 @@ router.get('/events', (req, res) => {
 
   const interval = setInterval(() => {
     const randomData = Math.random();
-    res.write(`data: ${JSON.stringify({ data: data })}\n\n`);
-  }, 2000); // Send data every 2 seconds
+    res.write(`data: ${JSON.stringify({ data: randomData })}\n\n`);
+  }, 1000); // Send data every 2 seconds
 
   req.on('close', () => {
     clearInterval(interval); 
