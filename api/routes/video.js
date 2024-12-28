582b2a487422cb4513586134a0ac3367988081c1
penguin <penguin@gmail.com>
2024-12-28 11:54:18 +0000

    adding docker-compose.yml

--- api/routes/video.js
+++ api/routes/video.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-
-
-
-
 
 router.get('/', function(req, res, next) {
 res.sendFile('alprVideo.mp4', { root: 'public/uploaded-videos'});
