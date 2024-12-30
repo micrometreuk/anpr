01072442b997a2e2a995b1a769c6b38fefee84f2
Henok Wehibe <henokwehibe@gmail.com>
2024-12-30 09:07:53 +0000

    bench-marking with

--- api/controllers/anpr.js
+++ api/controllers/anpr.js
@@ -5,7 +5,6 @@ const sse = new SSE();
 
 
 exports.index = async function (req, res) {
-    console.log((newAlprSee))
     res.send('index');
 
 };
@@ -14,13 +13,15 @@ exports.create = async function (req, res) {
     var anpr_host_protpcol = "http://"
     var anpr_images_path = "/alprd-images/"
     var anpr_image = req.body.uuid + ".jpg"
-    var anpr_host = req.rawHeaders[1]
+    var anpr_host = '127.0.0.1'
     var anpr_image_url = anpr_host_protpcol + anpr_host + anpr_images_path + anpr_image;
     var newAlpr = new Alpr({
         image: anpr_image_url,
         _id: req.body.results[0].plate,
 
     });
+    var ip = req.headers
+
     console.log((newAlpr))
     try {
         await newAlpr.save();
